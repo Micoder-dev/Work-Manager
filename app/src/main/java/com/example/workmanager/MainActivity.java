c1d2dea4cefddf3547e9609df71eec4830c41caa
Micoder-dev <micoder.com@gmail.com>
2022-08-29 23:09:06 +0530

    Display Worker Status

--- app/src/main/java/com/example/workmanager/MainActivity.java
+++ app/src/main/java/com/example/workmanager/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.workmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,6 +48,19 @@ public class MainActivity extends AppCompatActivity {
                 WorkManager.getInstance(getApplicationContext()).enqueue(countWorkRequest);
             }
         });
+
+
+        // Let's Display the Status of our Worker
+        WorkManager.getInstance(getApplicationContext()).getWorkInfoByIdLiveData(countWorkRequest.getId())
+                .observe(this, new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(WorkInfo workInfo) {
+                        if (workInfo != null) {
+                            Toast.makeText(MainActivity.this, "Status: "+workInfo.getState().name(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
 
     }
 }
